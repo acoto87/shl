db6979642c3eeef01f35783ed730bfeb214deeef
acoto87 <acoto87@gmail.com>
2018-10-02 18:11:04 -0500

    - added InsertRange, RemoveAtRange functions to list - now the lists is constructed with a set of options in which one specify the equalsFn, freeFn and other options.

--- list.h
+++ list.h
@@ -9,138 +9,236 @@
 #define shlDeclareList(typeName, itemType) \
     typedef struct \
     { \
+        itemType defaultValue; \
+        bool (*equalsFn)(const itemType item1, const itemType item2); \
+        void (*freeFn)(itemType item); \
+    } typeName ## Options; \
+    \
+    typedef struct \
+    { \
         uint32_t count; \
         uint32_t capacity; \
-        uint32_t loadFactor; \
-        itemType *items; \
+        bool (*equalsFn)(const itemType item1, const itemType item2); \
+        void (*freeFn)(itemType item); \
+        itemType defaultValue; \
+        itemType* items; \
     } typeName; \
     \
-    void typeName ## Init(typeName *list); \
-    void typeName ## Free(typeName *list); \
-    void typeName ## Add(typeName *list, itemType value); \
-    void typeName ## Insert(typeName *list, int32_t index, itemType value); \
-    int32_t typeName ## IndexOf(typeName *list, itemType value); \
-    itemType typeName ## Get(typeName *list, int32_t index); \
-    itemType typeName ## Set(typeName *list, int32_t index, itemType value); \
-    bool typeName ## Contains(typeName *list, itemType value); \
-    itemType typeName ## RemoveAt(typeName *list, int32_t index); \
-    itemType typeName ## Remove(typeName *list, itemType value);
+    void typeName ## Init(typeName* list, typeName ## Options options); \
+    void typeName ## Free(typeName* list); \
+    void typeName ## Add(typeName* list, itemType value); \
+    void typeName ## AddRange(typeName* list, int32_t count, itemType value[]); \
+    void typeName ## Insert(typeName* list, int32_t index, itemType value); \
+    void typeName ## InsertRange(typeName* list, int32_t index, int32_t count, itemType values[]); \
+    int32_t typeName ## IndexOf(typeName* list, itemType value); \
+    itemType typeName ## Get(typeName* list, int32_t index); \
+    void typeName ## Set(typeName* list, int32_t index, itemType value); \
+    bool typeName ## Contains(typeName* list, itemType value); \
+    void typeName ## Remove(typeName* list, itemType value); \
+    void typeName ## RemoveAt(typeName* list, int32_t index); \
+    void typeName ## RemoveAtRange(typeName* list, int32_t index, int32_t count); \
+    void typeName ## Reverse(typeName* list); \
+    void typeName ## Sort(typeName* list, int32_t (*compareFn)(const itemType item1, const itemType item2));
 
-#define shlDefineList(typeName, itemType, equalsFn, defaultValue) \
-    void typeName ## __resize(typeName *list) \
+#define shlDefineList(typeName, itemType) \
+    void typeName ## __resize(typeName* list, int32_t minSize) \
     { \
         uint32_t oldCapacity = list->capacity; \
-        uint32_t oldLoadFactor = list->loadFactor; \
         itemType *old = list->items; \
         \
-        list->loadFactor = oldLoadFactor << 1; \
         list->capacity = oldCapacity << 1; \
-        list->items = (itemType *)calloc(list->capacity, sizeof(itemType)); \
+        if (list->capacity < minSize) \
+            list->capacity = minSize; \
         \
-        memcpy(list->items, old, list->count * sizeof(itemType)); \
+        list->items = (itemType *)realloc(list->items, list->capacity * sizeof(itemType)); \
     } \
     \
-    void typeName ## Init(typeName *list) \
+    void typeName ## __qsort(typeName* list, int32_t left, int32_t right, int32_t (*compareFn)(const itemType item1, const itemType item2)) \
+    { \
+        if (left >= right) \
+            return; \
+        \
+        int32_t middle = left + ((right - left) >> 1); \
+        itemType p = list->items[middle]; \
+        \
+        int32_t i = left - 1; \
+        int32_t j = right + 1; \
+        \
+        while (i < j) \
+        { \
+            do { i++; } while (compareFn(list->items[i], p) < 0); \
+            do { j--; } while (compareFn(list->items[j], p) > 0); \
+            \
+            if (i >= j) \
+                break; \
+            \
+            itemType tmp = list->items[i]; \
+            list->items[i] = list->items[j]; \
+            list->items[j] = tmp; \
+        } \
+        \
+        typeName ## __qsort(list, left, j, compareFn); \
+        typeName ## __qsort(list, j + 1, right, compareFn); \
+    } \
+    \
+    typeName ## Options typeName ## DefaultOptions() \
+    { \
+        return (typeName ## Options){0}; \
+    } \
+    \
+    void typeName ## Init(typeName* list, typeName ## Options options) \
     { \
         list->capacity = 8; \
-        list->loadFactor = 8; \
+        list->defaultValue = options.defaultValue; \
+        list->equalsFn = options.equalsFn; \
+        list->freeFn = options.freeFn; \
         list->count = 0; \
-        list->items = (itemType *)calloc(list->capacity, sizeof(itemType)); \
+        list->items = (itemType *)malloc(list->capacity * sizeof(itemType)); \
     } \
     \
-    void typeName ## Free(typeName *list) \
+    void typeName ## Free(typeName* list) \
     { \
+        if (!list->items) \
+            return; \
+        \
+        if (list->freeFn) \
+        { \
+            for(int32_t i = 0; i < list->count; i++) \
+                list->freeFn(list->items[i]); \
+        } \
+        \
         free(list->items); \
         list->items = 0; \
         list->count = 0; \
     } \
     \
-    void typeName ## Add(typeName *list, itemType value) \
+    void typeName ## InsertRange(typeName* list, int32_t index, int32_t count, itemType values[]) \
+    { \
+        if (!list->items) \
+            return; \
+        \
+        if (index < 0 || index > list->count) \
+            return; \
+        \
+        if (list->count + count >= list->capacity) \
+            typeName ## __resize(list, list->count + count); \
+        \
+        memmove(list->items + index + count, list->items + index, (list->count - index) * sizeof(itemType)); \
+        memcpy(list->items + index, values, count * sizeof(itemType)); \
+        list->count += count; \
+    } \
+    \
+    void typeName ## Insert(typeName* list, int32_t index, itemType value) \
+    { \
+        typeName ## InsertRange(list, index, 1, &value); \
+    } \
+    \
+    void typeName ## Add(typeName* list, itemType value) \
     { \
         typeName ## Insert(list, list->count, value); \
     } \
     \
-    void typeName ## Insert(typeName *list, int32_t index, itemType value) \
+    void typeName ## AddRange(typeName* list, int32_t count, itemType values[]) \
     { \
-        if (!list->items) \
-            return; \
-        \
-        if (index < 0 && index > list->count) \
-            return; \
-        \
-        if (list->count == list->loadFactor) \
-            typeName ## __resize(list); \
-        \
-        memmove(list->items + index + 1, list->items + index, (list->count - index) * sizeof(itemType)); \
-        \
-        list->items[index] = value; \
-        list->count++; \
+        typeName ## InsertRange(list, list->count, count, values); \
     } \
     \
-    int32_t typeName ## IndexOf(typeName *list, itemType value) \
+    int32_t typeName ## IndexOf(typeName* list, itemType value) \
     { \
         if (!list->items) \
             return -1; \
         \
+        if (!list->equalsFn) \
+            return -1; \
+        \
         for(int32_t i = 0; i < list->count; i++) \
         { \
-            if (equalsFn(list->items[i], value)) \
+            if (list->equalsFn(list->items[i], value)) \
                 return i; \
         } \
         \
         return -1; \
     } \
     \
-    bool typeName ## Contains(typeName *list, itemType value) \
+    bool typeName ## Contains(typeName* list, itemType value) \
     { \
         return typeName ## IndexOf(list, value) >= 0; \
     } \
     \
-    itemType typeName ## Get(typeName *list, int32_t index) \
+    itemType typeName ## Get(typeName* list, int32_t index) \
     { \
         if (!list->items) \
-            return defaultValue; \
+            return list->defaultValue; \
         \
-        if (index < 0 && index >= list->count) \
-            return defaultValue; \
+        if (index < 0 || index >= list->count) \
+            return list->defaultValue; \
         \
         return list->items[index]; \
     } \
     \
-    itemType typeName ## Set(typeName *list, int32_t index, itemType value) \
+    void typeName ## Set(typeName *list, int32_t index, itemType value) \
     { \
         if (!list->items) \
-            return defaultValue; \
+            return; \
         \
-        if (index < 0 && index >= list->count) \
-            return defaultValue; \
+        if (index < 0 || index >= list->count) \
+            return; \
         \
         itemType currentValue = list->items[index]; \
+        if (list->freeFn) \
+            list->freeFn(currentValue); \
+        \
         list->items[index] = value; \
-        return currentValue; \
     } \
     \
-    itemType typeName ## RemoveAt(typeName *list, int32_t index) \
+    void typeName ## RemoveAtRange(typeName *list, int32_t index, int32_t count) \
     { \
         if (!list->items) \
-            return defaultValue; \
+            return; \
         \
-        if (index < 0 && index >= list->count) \
-            return defaultValue; \
+        if (index < 0 || index >= list->count) \
+            return; \
         \
-        itemType value = list->items[index]; \
-        memmove(list->items + index, list->items + index + 1, (list->count - index - 1) * sizeof(itemType)); \
-        list->count--; \
-        return value; \
+        if (index + count > list->count) \
+            return; \
+        \
+        if (list->freeFn) \
+        { \
+            for(int32_t i = 0; i < count; i++) \
+                list->freeFn(list->items[index + i]); \
+        } \
+        \
+        memmove(list->items + index, list->items + index + count, (list->count - index - count) * sizeof(itemType)); \
+        list->count -= count; \
     } \
     \
-    itemType typeName ## Remove(typeName *list, itemType value) \
+    void typeName ## RemoveAt(typeName *list, int32_t index) \
+    { \
+        typeName ## RemoveAtRange(list, index, 1); \
+    } \
+    \
+    void typeName ## Remove(typeName *list, itemType value) \
     { \
         int32_t index = typeName ## IndexOf(list, value); \
-        if (index < 0) \
-            return defaultValue; \
+        typeName ## RemoveAt(list, index); \
+    } \
+    \
+    void typeName ## Reverse(typeName *list) \
+    { \
+        if (!list->items) \
+            return; \
         \
-        return typeName ## RemoveAt(list, index); \
+        int32_t count = list->count; \
+        for(int32_t i = 0; i < count / 2; i++) \
+        { \
+            itemType tmp = list->items[i]; \
+            list->items[i] = list->items[count - i - 1]; \
+            list->items[count - i - 1] = tmp; \
+        } \
+    } \
+    void typeName ## Sort(typeName* list, int32_t (*compareFn)(const itemType item1, const itemType item2)) \
+    { \
+        typeName ## __qsort(list, 0, list->count - 1, compareFn); \
     }
 
 #endif // SHL_LIST_H
--- tests/list_test.c
+++ tests/list_test.c
@@ -19,15 +19,23 @@ bool intEquals(const int x, const int y)
     return x == y;
 }
 
+int32_t intCompare(const int x, const int y)
+{
+    return x - y;
+}
+
 shlDeclareList(IntList, int)
-shlDefineList(IntList, int, intEquals, 0)
+shlDefineList(IntList, int)
 
 void valueTypeTest()
 {
     float start, end;
 
+    IntListOptions options = IntListDefaultOptions();
+    options.equalsFn = intEquals;
+
     IntList list;
-    IntListInit(&list);
+    IntListInit(&list, options);
 
     printf("--- Start value type tests ---\n");
 
@@ -64,8 +72,8 @@ void valueTypeTest()
     {
         int index = rand() % list.count;
         int value = list.items[index];
-        int deletedValue = IntListRemoveAt(&list, index);
-        assert(deletedValue == value);
+        IntListRemoveAt(&list, index);
+        assert(list.items[index] != value);
     }
     end = getTime();
     printf("List count and capacity: (%d, %d)\n", list.count, list.capacity);
@@ -102,6 +110,50 @@ void valueTypeTest()
     printf("Time: %.2f seconds\n", end - start);
     printf("--- End test 5: insert %d objects ---\n", count / 2);
 
+    printf("\n");
+
+    const int rangeCount = 50000;
+    const int rangeAt = 100;
+
+    int* rangeValues = (int*)malloc(rangeCount*sizeof(int));
+    for(int i = 0; i < rangeCount; i++)
+        rangeValues[i] = -i;
+
+    printf("--- Start test 6: insert range %d objects ---\n", rangeCount);
+    start = getTime();
+    IntListInsertRange(&list, rangeAt, rangeCount, rangeValues);
+    end = getTime();
+    for(int i = 0; i < rangeCount; i++)
+        assert(list.items[rangeAt + i] == rangeValues[i]);
+    printf("List count and capacity: (%d, %d)\n", list.count, list.capacity);
+    printf("Time: %.2f seconds\n", end - start);
+    printf("--- End test 6: insert range %d objects ---\n", rangeCount);
+
+    printf("\n");
+
+    printf("--- Start test 7: remove range %d objects ---\n", rangeCount);
+    start = getTime();
+    IntListRemoveAtRange(&list, rangeAt, rangeCount);
+    end = getTime();
+    for(int i = 0; i < rangeCount; i++)
+        assert(list.items[rangeAt + i] != rangeValues[i]);
+    printf("List count and capacity: (%d, %d)\n", list.count, list.capacity);
+    printf("Time: %.2f seconds\n", end - start);
+    printf("--- End test 7: remove range %d objects ---\n", rangeCount);
+
+    free(rangeValues);
+
+    printf("\n");
+
+    printf("--- Start test 8: sorting %d objects ---\n", list.count);
+    start = getTime();
+    IntListSort(&list, intCompare);
+    end = getTime();
+    for(int i = 1; i < list.count; i++)
+        assert(list.items[i - 1] <= list.items[i]);
+    printf("Time: %.2f seconds\n", end - start);
+    printf("--- End test 8: sorting %d objects ---\n", list.count);
+
     printf("--- End value type tests ---\n");
 }
 
@@ -114,23 +166,41 @@ typedef struct
 
 bool EntryEquals(const Entry *e1, const Entry *e2)
 {
-    return e1->index == e2->index &&
-           strcmp(e1->name, e2->name) == 0;
+    return e1 == e2;
+}
+
+int32_t EntryCompare(const Entry *e1, const Entry *e2)
+{
+    if (e1->index == e2->index)
+        return strcmp(e1->name, e2->name);
+    return e1->index - e2->index;
+}
+
+void EntryFree(Entry* e)
+{
+    free(e);
 }
 
 shlDeclareList(EntriesList, Entry*)
-shlDefineList(EntriesList, Entry*, EntryEquals, NULL)
+shlDefineList(EntriesList, Entry*)
 
 void referenceTypeTest()
 {
     float start, end;
+    Entry** entries;
+
+    EntriesListOptions options = EntriesListDefaultOptions();
+    options.defaultValue = NULL;
+    options.equalsFn = EntryEquals;
+    options.freeFn = EntryFree;
 
     EntriesList list;
-    EntriesListInit(&list);
+    EntriesListInit(&list, options);
 
     printf("--- Start reference type tests ---\n");
 
     printf("--- Start test 1: add %d objects ---\n", count);
+    entries = malloc(count * sizeof(Entry*));
     start = getTime();
     for(int i = 0; i < count; i++)
     {
@@ -139,6 +209,7 @@ void referenceTypeTest()
         entry->name = "entry";
         EntriesListAdd(&list, entry);
         assert(list.items[list.count - 1]->index == i);
+        entries[i] = entry;
     }
     end = getTime();
     printf("List count and capacity: (%d, %d)\n", list.count, list.capacity);
@@ -149,57 +220,20 @@ void referenceTypeTest()
 
     printf("--- Start test 2: contains %d objects ---\n", count / 2);
     start = getTime();
-    for(int i = 0; i < count/2; i++)
+    for(int i = 0; i < count / 2; i++)
     {
-        Entry *entry = (Entry*)malloc(sizeof(Entry));
-        entry->index = rand() % count;
-        entry->name = "entry";
+        Entry *entry = entries[rand() % count];
         assert(EntriesListContains(&list, entry));
-        free(entry);
     }
     end = getTime();
     printf("Time: %.2f seconds\n", end - start);
     printf("--- End test 2: contains %d objects ---\n", count / 2);
 
-    printf("\n");
-
-    printf("--- Start test 3: remove by index %d objects ---\n", count / 2);
-    start = getTime();
-    for(int i = 0; i < count/2; i++)
-    {
-        int index = rand() % list.count;
-        Entry *entry = list.items[index];
-        Entry *deletedEntry = EntriesListRemoveAt(&list, index);
-        assert(deletedEntry->index == entry->index);
-        free(deletedEntry);
-    }
-    end = getTime();
-    printf("List count and capacity: (%d, %d)\n", list.count, list.capacity);
-    printf("Time: %.2f seconds\n", end - start);
-    printf("--- End test 3: remove by index %d objects ---\n", count / 2);
+    free(entries);
 
     printf("\n");
 
-    printf("--- Start test 4: remove by value %d objects ---\n", count / 2);
-    start = getTime();
-    for(int i = 0; i < count/2; i++)
-    {
-        Entry *entry = (Entry*)malloc(sizeof(Entry));
-        entry->index = rand() % count;
-        entry->name = "entry";
-        Entry *deletedEntry = EntriesListRemove(&list, entry);
-        assert(!EntriesListContains(&list, deletedEntry));
-        free(deletedEntry);
-        free(entry);
-    }
-    end = getTime();
-    printf("List count and capacity: (%d, %d)\n", list.count, list.capacity);
-    printf("Time: %.2f seconds\n", end - start);
-    printf("--- End test 4: remove by value %d objects ---\n", count / 2);
-
-    printf("\n");
-
-    printf("--- Start test 5: insert %d objects ---\n", count / 2);
+    printf("--- Start test 3: insert %d objects ---\n", count / 2);
     start = getTime();
     for(int i = 0; i < count/2; i++)
     {
@@ -212,7 +246,96 @@ void referenceTypeTest()
     end = getTime();
     printf("List count and capacity: (%d, %d)\n", list.count, list.capacity);
     printf("Time: %.2f seconds\n", end - start);
-    printf("--- End test 5: insert %d objects ---\n", count / 2);
+    printf("--- End test 3: insert %d objects ---\n", count / 2);
+
+    printf("\n");
+
+    printf("--- Start test 4: remove by index %d objects ---\n", count / 2);
+    start = getTime();
+    for(int i = 0; i < count/2; i++)
+    {
+        int index = rand() % (list.count - 1);
+        Entry* entry = list.items[index + 1];
+        EntriesListRemoveAt(&list, index);
+        assert(EntryEquals(list.items[index], entry));
+    }
+    end = getTime();
+    printf("List count and capacity: (%d, %d)\n", list.count, list.capacity);
+    printf("Time: %.2f seconds\n", end - start);
+    printf("--- End test 4: remove by index %d objects ---\n", count / 2);
+
+    printf("\n");
+
+    printf("--- Start test 5: remove by value %d objects ---\n", count / 2);
+    
+    entries = malloc((count / 2) * sizeof(Entry*));
+    for(int i = 0; i < count / 2; i++)
+    {
+        Entry* entry = (Entry*)malloc(sizeof(Entry));
+        entry->index = rand() % list.count;
+        entry->name = "negative";
+        EntriesListInsert(&list, entry->index, entry);
+        entries[i] = entry;
+    }
+    start = getTime();
+    for(int i = 0; i < count / 2; i++)
+        EntriesListRemove(&list, entries[i]);
+    end = getTime();
+    for(int i = 0; i < list.count; i++)
+        assert(strcmp(list.items[i]->name, "negative") != 0);
+    printf("List count and capacity: (%d, %d)\n", list.count, list.capacity);
+    printf("Time: %.2f seconds\n", end - start);
+    printf("--- End test 5: remove by value %d objects ---\n", count / 2);
+
+    free(entries);
+
+    printf("\n");
+
+    const int rangeCount = 50000;
+    const int rangeAt = 100;
+
+    Entry** rangeValues = (Entry**)malloc(rangeCount * sizeof(Entry*));
+    for(int i = 0; i < rangeCount; i++)
+    {
+        rangeValues[i] = malloc(sizeof(Entry));
+        rangeValues[i]->index = -i;
+        rangeValues[i]->name = "insert range";
+    }
+
+    printf("--- Start test 6: insert range %d objects ---\n", rangeCount);
+    start = getTime();
+    EntriesListInsertRange(&list, rangeAt, rangeCount, rangeValues);
+    end = getTime();
+    for(int i = 0; i < rangeCount; i++)
+        assert(EntryEquals(list.items[rangeAt + i], rangeValues[i]));
+    printf("List count and capacity: (%d, %d)\n", list.count, list.capacity);
+    printf("Time: %.2f seconds\n", end - start);
+    printf("--- End test 6: insert range %d objects ---\n", rangeCount);
+
+    printf("\n");
+
+    printf("--- Start test 7: remove range %d objects ---\n", rangeCount);
+    start = getTime();
+    EntriesListRemoveAtRange(&list, rangeAt, rangeCount);
+    end = getTime();
+    for(int i = 0; i < rangeCount; i++)
+        assert(list.items[rangeAt + i]->index >= 0);
+    printf("List count and capacity: (%d, %d)\n", list.count, list.capacity);
+    printf("Time: %.2f seconds\n", end - start);
+    printf("--- End test 7: remove range %d objects ---\n", rangeCount);
+
+    free(rangeValues);
+
+    printf("\n");
+
+    printf("--- Start test 8: sorting %d objects ---\n", list.count);
+    start = getTime();
+    EntriesListSort(&list, EntryCompare);
+    end = getTime();
+    for(int i = 1; i < list.count; i++)
+        assert(EntryCompare(list.items[i - 1], list.items[i]) <= 0);
+    printf("Time: %.2f seconds\n", end - start);
+    printf("--- End test 8: sorting %d objects ---\n", list.count);
 
     printf("--- End reference type tests ---\n");
 }
